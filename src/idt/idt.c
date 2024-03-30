ce5d599f22ce8e4db29a350d3ed1719bda21ec0c
Mia <miabernhardt75@gmail.com>
2024-03-30 17:28:33 +0100

    setup PIC

--- src/idt/idt.c
+++ src/idt/idt.c
@@ -1,7 +1,6 @@
-#include "util/types.h"
 #include "idt.h"
 #include "interrupts.h"
-
+#include "util/port.h"
 
 IDTR idtr;
 IDTDescEntry IDT[256];
@@ -20,16 +19,61 @@ void set_isr_function(u8 index, ISRFunction func) {
     isr_functions[index] = func;
 }
 
+#define PIC1           0x20
+#define PIC2           0xA0
+#define PIC1_COMMAND   PIC1
+#define PIC2_COMMAND   PIC2
+#define PIC1_DATA      (PIC1 + 1) // 0x21
+#define PIC2_DATA      (PIC2 + 1) // 0xA1
+
+#define PIC_EOI        0x20       // end of input
+
+#define ICW1_ICW4      0x01
+#define ICW1_SINGLE    0x02
+#define ICW1_INTERVAL4 0x04
+#define ICW1_LEVEL     0x08
+#define ICW1_INIT      0x10
+
+#define ICW4_8086      0x01
+
+void setup_irq() {
+    u8 a1, a2;
+    a1 = inb(PIC1_DATA);
+    a2 = inb(PIC2_DATA);
+
+    outb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4); // 0x11
+    outb(PIC1_DATA, 32);                       // offset
+    outb(PIC1_DATA, 0x04);                     // slave PIC at IRQ2
+    outb(PIC1_DATA, ICW4_8086);
+    outb(PIC1_DATA, a1);
+
+    outb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4); // 0x11
+    outb(PIC2_DATA, 40);                       // offset
+    outb(PIC2_DATA, 0x02);                     // slave
+    outb(PIC2_DATA, ICW4_8086);
+    outb(PIC2_DATA, a2);
+}
+
 void prepare_idt() {
     idtr.limit = 0x0FFF;
     idtr.ptr = IDT;
+
+    setup_irq();
 
     for (int i = 0; i < 48; i++) {
         idt_set_entry(idtr.ptr, i, isr_redirect_table[i], TA_INTERRUPT);
     }
 
+    /*idt_set_entry(idtr.ptr, 0xE, page_fault_handler, TA_INTERRUPT);*/
+
     asm ("lidt %0" : : "m" (idtr));
 }
+
 void handle_interrupt(InterruptFrame* frame) {
-    (void)frame;
+    if (frame->interrupt >= 32 && frame->interrupt <= 47) {
+        if (frame->interrupt >= 40) {
+            outb(PIC2, PIC_EOI);
+        }
+        outb(PIC1, PIC_EOI);
+    }
 }
