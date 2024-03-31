896db5c18acda10eb90dd6e2100a1e0a8504e041
Mia <miabernhardt75@gmail.com>
2024-03-31 23:47:35 +0200

    setup raw paging

--- src/boot.asm
+++ src/boot.asm
@@ -1,39 +1,77 @@
 extern kernel_main
 
+; ===============================================
+; MULTIBOOT
 SECTION .multiboot
 align 4
 
+%define MBOOT_PAGE_ALIGN (1 << 0)
+%define MBOOT_MEMINFO    (1 << 1)
+%define MBOOT_USE_GFX    (0)
+
 %define MAGIC 0x1badb002
-%define ALIGN 1<<0
-%define MEMINFO 1<<1
-%define FLAGS (ALIGN | MEMINFO)
+%define FLAGS (MBOOT_PAGE_ALIGN | MBOOT_MEMINFO | MBOOT_USE_GFX)
 %define CHECKSUM (-(MAGIC + FLAGS))
 
 dd MAGIC
 dd FLAGS
 dd CHECKSUM
+dd 0, 0, 0, 0, 0
 
+; graphics
+dd 0
+dd 800     ; x-size
+dd 600     ; y-size
+dd 32      ; depth
+
+; ===============================================
+; BSS
 SECTION .bss
 align 16
 
 stack_bottom:
-resb 16384
+resb 0x20000
 stack_top:
 
-
-SECTION .text
-
-%include "./src/gdt/gdt.asm"
-%include "./src/idt/load_idt.asm"
-%include "./src/idt/interrupts_defines.asm"
+; ===============================================
+; BOOT
+SECTION .boot
 
 global _start
 _start:
+    ; save multiboot info
+    mov [multiboot_info], ebx
+    mov [multiboot_magic], eax
+
+    mov eax, (initial_page_dir - 0xC0000000)
+    mov cr3, eax
+
+    ; turn on physical address extension
+    ; cr4 |= 0x10
+    mov ecx, cr4
+    or ecx, 0x10
+    mov cr4, ecx
+
+    ; enable paging
+    ; cr0 |= 0x80000000
+    mov ecx, cr0
+    or ecx, 0x80000000
+    mov cr0, ecx
+    
+    jmp higher_half
+
+; ===============================================
+; TEXT
+SECTION .text
+
+higher_half:
     mov esp, stack_top
 
-    ;int 0x0e                ; trigger "page fault" to test IDT
+    xor ebp, ebp
 
-    push ebx
+    mov eax, [multiboot_info]
+    push eax
+    mov eax, [multiboot_magic]
     push eax
 
     cli
@@ -43,3 +81,28 @@ halt:
     cli
     hlt
     jmp halt
+
+%include "./src/gdt/gdt.asm"
+%include "./src/idt/load_idt.asm"
+%include "./src/idt/interrupts_defines.asm"
+
+; ===============================================
+; DATA
+SECTION .data
+align 4096
+
+global initial_page_dir
+initial_page_dir:
+    dd 10000011b
+
+    times 768-1 dd 0
+
+    dd (0 << 22) | 10000011b
+    dd (1 << 22) | 10000011b
+    dd (2 << 22) | 10000011b
+    dd (3 << 22) | 10000011b
+
+    times 256-4 dd 0
+
+multiboot_info: dd 0
+multiboot_magic: dd 0
