5b5e3332cd78fad4b8db605daa945161a4a06d17
Mia <miabernhardt75@gmail.com>
2024-04-21 15:17:51 +0200

    added sleep()

--- src/util/sys.c
+++ src/util/sys.c
@@ -1,5 +1,6 @@
 #include "sys.h"
 
+#include "pit/pit.h"
 #include "util/port.h"
 
 void spin_halt() {
@@ -21,4 +22,11 @@ void reboot() {
     while (good & 0x02) good = inb(0x64);
     outb(0x64, 0xFE);
     halt();
+}
+
+void sleep(u32 ms) {
+    asm volatile("sti");
+    u64 tics = pit_get_tics();
+    while ((tics + ms) > pit_get_tics()) {
+    }
 }
--- src/util/sys.h
+++ src/util/sys.h
@@ -1,9 +1,11 @@
 #ifndef SYS_H_
 #define SYS_H_
 
+#include "util/types.h"
 void spin_halt();
 void halt();
 void abort();
 void reboot();
+void sleep(u32 ms);
 
 #endif
