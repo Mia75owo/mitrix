49ecc66bdf6b743d3d97d841ca3eb8a548fc0f7a
Mia <miabernhardt75@gmail.com>
2024-05-27 15:48:05 +0200

    added support for big allocations

--- src/memory/kmalloc.c
+++ src/memory/kmalloc.c
@@ -4,6 +4,11 @@
 #include "util/debug.h"
 #include "util/mem.h"
 
+static void print_buddy(u32* buddy, u32 num_bits, u32 max_bits);
+static u32 big_allocation_size(u32 chunk);
+static void big_allocation_remove(u32 chunk);
+static void big_allocation_add(u32 chunk, u32 length);
+
 #define EXACT_ALLOC_SIZE (1024 * 128)
 
 #define BUDDY64_RAW_SIZE (EXACT_ALLOC_SIZE / 64 / 32)
@@ -17,12 +22,20 @@ static u32 buddy4096[BUDDY4096_RAW_SIZE];
 static u32 allocated_by_user512[BUDDY512_RAW_SIZE];
 static u32 allocated_by_user4096[BUDDY512_RAW_SIZE];
 
+#define BIG_ALLOCATIONS_SIZE 64
+static struct {
+    u32 start;
+    u32 length;
+} big_allocations[BIG_ALLOCATIONS_SIZE];
+
 static bool initialized = false;
 
 void kmalloc_init() {
     memset(buddy64, 0, sizeof(buddy64));
     memset(buddy512, 0, sizeof(buddy512));
     memset(buddy4096, 0, sizeof(buddy4096));
+
+    memset(big_allocations, 0, sizeof(big_allocations));
 
     initialized = true;
 }
@@ -108,7 +121,38 @@ u8* kmalloc(u32 size) {
 
         return (u8*)(KERNEL_MALLOC + found_index * 4096);
     } else {
-        assert_msg(0, "KMALLOC: (size > 4096)") return NULL;
+        u32 chunk_count = size / 4096;
+
+        u32 found_length = 0;
+        i32 found_index = -1;
+        for (u32 i = 0; i < BUDDY4096_RAW_SIZE * 32; i++) {
+            if (BITMAP_GET(buddy4096, i) == 0) {
+                found_length++;
+                if (found_length >= chunk_count) {
+                    found_index = i - (found_length - 1);
+                    break;
+                }
+            } else {
+                found_length = 0;
+            }
+        }
+
+        assert_msg(found_index != -1, "KMALLOC: no place for big allocation");
+
+        for (u32 chunk = 0; chunk < chunk_count; chunk++) {
+            // Update buddy bitmaps
+            for (u32 i = 0; i < 64; i++) {
+                BITMAP_SET(buddy64, (found_index + chunk) * 64 + i, 1);
+            }
+            for (u32 i = 0; i < 8; i++) {
+                BITMAP_SET(buddy512, (found_index + chunk) * 8 + i, 1);
+            }
+            BITMAP_SET(buddy4096, (found_index + chunk), 1);
+            BITMAP_SET(allocated_by_user4096, (found_index + chunk), 1);
+        }
+
+        big_allocation_add(found_index, chunk_count);
+        return (u8*)(KERNEL_MALLOC + found_index * 4096);
     }
 }
 
@@ -123,17 +167,28 @@ void kfree(u8* addr) {
 
     // Was allocated as a 4096 chunk?
     if (BITMAP_GET(allocated_by_user4096, relative_addr / 4096)) {
-        BITMAP_SET(allocated_by_user4096, relative_addr / 4096, 0);
+        // Get chunk count (could be multiple 4096 chunks)
+        u32 chunk_count = big_allocation_size(relative_addr / 4096);
+        for (u32 chunk = 0; chunk < big_allocation_size(relative_addr / 4096);
+             chunk++) {
+            u32 current_addr = (relative_addr / 4096 + chunk) * 4096;
 
-        // Mark 4096 as free
-        BITMAP_SET(buddy4096, relative_addr / 4096, 0);
-        // Mark 512 as free
-        for (u32 i = 0; i < (4096 / 512); i++) {
-            BITMAP_SET(buddy512, (relative_addr / 512) + i, 0);
+            BITMAP_SET(allocated_by_user4096, current_addr / 4096, 0);
+
+            // Mark 4096 as free
+            BITMAP_SET(buddy4096, current_addr / 4096, 0);
+            // Mark 512 as free
+            for (u32 i = 0; i < (4096 / 512); i++) {
+                BITMAP_SET(buddy512, (current_addr / 512) + i, 0);
+            }
+            // Mark 64 as free
+            for (u32 i = 0; i < (4096 / 64); i++) {
+                BITMAP_SET(buddy64, (current_addr / 64) + i, 0);
+            }
         }
-        // Mark 64 as free
-        for (u32 i = 0; i < (4096 / 64); i++) {
-            BITMAP_SET(buddy64, (relative_addr / 64) + i, 0);
+        // Remove from big allocation index
+        if (chunk_count != 1) {
+            big_allocation_remove(relative_addr / 4096);
         }
         return;
     }
@@ -161,7 +216,7 @@ void kfree(u8* addr) {
         }
     }
     // Mark as free
-    BITMAP_SET(buddy4096, relative_addr / 4096, 0);
+    BITMAP_SET(buddy512, relative_addr / 512, 0);
 
 check_4096_free:
     for (u32 i = 0; i < (4096 / 512); i++) {
@@ -192,6 +247,36 @@ static void print_buddy(u32* buddy, u32 num_bits, u32 max_bits) {
             }
         } else {
             break;
+        }
+    }
+}
+
+static u32 big_allocation_size(u32 chunk) {
+    for (u32 i = 0; i < BIG_ALLOCATIONS_SIZE; i++) {
+        if (big_allocations[i].start == chunk &&
+            big_allocations[i].length != 0) {
+            return big_allocations[i].length;
+        }
+    }
+    return 1;
+}
+static void big_allocation_add(u32 chunk, u32 length) {
+    for (u32 i = 0; i < BIG_ALLOCATIONS_SIZE; i++) {
+        if (big_allocations[i].length == 0) {
+            big_allocations[i].start = chunk;
+            big_allocations[i].length = length;
+            return;
+        }
+    }
+    assert_msg(0, "KMALLOC: no place in BIG_ALLOCATION index");
+}
+static void big_allocation_remove(u32 chunk) {
+    for (u32 i = 0; i < BIG_ALLOCATIONS_SIZE; i++) {
+        if (big_allocations[i].start == chunk &&
+            big_allocations[i].length != 0) {
+            big_allocations[i].start = 0;
+            big_allocations[i].length = 0;
+            return;
         }
     }
 }
