1ac0b412e858d1e4f11705d1c3567dc1560e069a
Mia <miabernhardt75@gmail.com>
2024-05-30 15:43:29 +0200

    added baselibc

--- userspace/utest/main.c
+++ userspace/utest/main.c
@@ -1,7 +1,12 @@
 #include "syscalls.h"
+#include "stdio.h"
 
 int _start() {
-    syscall_print("userspace/utest/utest.c :)");
+    char buf[256];
+    snprintf(buf, 256, "LIBC :D %i\n", 1234) ;
+
+    // syscall_print("userspace/utest/utest.c :)");
+    syscall_print(buf);
     syscall_print_char('\n');
     syscall_exit();
 
